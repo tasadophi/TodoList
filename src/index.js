fc770959ae57c1a0f41f0c1a2f795e9293e4c55e
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-01 14:44:14 +0330

    reset storage data added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-
+localStorage.setItem("filterBy", "filterAll");
+localStorage.removeItem("filter");
 ReactDOM.render(<App />, document.getElementById("root"));
