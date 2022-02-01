8fb7bf8fca4244bfbc16d72f13e19598b3a01751
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-01 14:40:51 +0330

    TodoList component completed

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,16 @@
 import Todo from "../Todo/Todo";
+import { getAllTodos, useTodos } from "../TodoApp";
 import styles from "./TodoList.module.css";
 const TodoList = () => {
+  let todos = useTodos();
+  if (!localStorage.getItem("filter")) {
+    todos = getAllTodos();
+  }
+  const renderTodos = () =>
+    todos.map((todo) => <Todo key={todo.id} todo={todo} />);
   return (
     <section className={`${styles.todos} container`}>
-      <Todo />
-      <Todo />
-      <Todo />
+      {todos.length > 0 ? renderTodos() : <div className={styles.empty}>این لیست خالی می باشد!</div>}
     </section>
   );
 };
