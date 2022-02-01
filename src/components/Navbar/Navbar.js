5b4f6e6c6fe653087751d6ffe005a3ced7da67be
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-01 14:40:10 +0330

    Navbar component completed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
 import styles from "./Navbar.module.css";
 import Select from "react-select";
 import { useState } from "react";
+import { getAllTodos, useTodos, useTodosDispatch } from "../TodoApp";
 
 const options = [
-  { value: "all", label: "همه" },
-  { value: "done", label: "انجام شده" },
-  { value: "undone", label: "انجام نشده" },
+  { value: "filterAll", label: "همه" },
+  { value: "filterDone", label: "انجام شده" },
+  { value: "filterUndone", label: "انجام نشده" },
 ];
 
 const Navbar = () => {
   const [value, setValue] = useState(options[0]);
+  const allTodos = getAllTodos();
+  const dispatch = useTodosDispatch();
+  const all = allTodos.length;
+  const done = allTodos.filter((todo) => todo.done).length;
+  const undone = allTodos.filter((todo) => !todo.done).length;
+
   const changeHandler = (e) => {
     setValue(e);
+    localStorage.setItem("filter", "true");
+    localStorage.setItem("filterBy", e.value);
+    dispatch({ type: e.value });
   };
 
   return (
@@ -19,9 +29,9 @@ const Navbar = () => {
       <h1>یادداشت ها</h1>
       <div className={styles.titles}>
         <Select value={value} onChange={changeHandler} options={options} />
-        <span className={styles.title}>تعداد کل : ۵</span>
-        <span className={styles.title}>انجام شده : ۵</span>
-        <span className={styles.title}>انجام نشده : ۵</span>
+        <span className={styles.title}>تعداد کل : {all}</span>
+        <span className={styles.title}>انجام شده : {done}</span>
+        <span className={styles.title}>انجام نشده : {undone}</span>
       </div>
     </header>
   );
