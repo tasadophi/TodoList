f5eeee3641bf03ab806c7e0da3e3623de56dbb10
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-09 19:55:07 +0330

    created and updated added

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -3,16 +3,23 @@ import { FaEdit } from "react-icons/fa";
 import { FaEraser } from "react-icons/fa";
 import { useModal, useTodosDispatch } from "../TodoApp";
 import { toast } from "react-toastify";
+
 const Todo = ({ todo }) => {
   const dispatch = useTodosDispatch();
   const { setShowModal } = useModal();
+
+  const getPersian = (todo) => {
+    const created = new Date(todo.created).toLocaleString("fa-IR");
+    const updated = new Date(todo.updated).toLocaleString("fa-IR");
+    return { created, updated };
+  };
 
   // handlers
   const deleteHandler = (e) => {
     dispatch({ type: "delete", id: e.currentTarget.id });
     if (localStorage.getItem("filterBy"))
       dispatch({ type: localStorage.getItem("filterBy") });
-    toast.warn("حذف شد!")
+    toast.warn("حذف شد!");
   };
 
   const editHandler = () => {
@@ -27,13 +34,25 @@ const Todo = ({ todo }) => {
 
   return (
     <div className={styles.todo}>
-      <span
-        className={`${styles.todoTitle} ${todo.done ? styles.done : ""}`}
-        onClick={checkHandler}
-        id={todo.id}
-      >
-        {todo.text}
-      </span>
+      <div className={styles.title}>
+        <span
+          className={`${styles.todoTitle} ${todo.done ? styles.done : ""}`}
+          onClick={checkHandler}
+          id={todo.id}
+        >
+          {todo.text}
+        </span>
+        <span className={styles.date}>
+          اضافه شده در : {getPersian(todo).created}
+        </span>
+        {!getPersian(todo).updated.includes("Invalid") ? (
+          <span className={styles.date}>
+            آخرین ویرایش : {getPersian(todo).updated}
+          </span>
+        ) : (
+          ""
+        )}
+      </div>
       <div className={styles.actions}>
         <span
           className={`${styles.icon} ${styles.delete}`}
